3a6f121c7de4a5ea6c0dc16f992f0d7993e7e17a
HieuNghia2k1 <boybin1707@gmail.com>
2024-10-15 00:24:34 +0700

    15/10

---
+++ src/components/Section/Many_lesion.jsx
@@ -0,0 +1,211 @@
+import React from 'react'
+import './Many_lesion.scss'
+import Slider from "react-slick";
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const Many_lesion = () => {
+    // class Many_lesion extends Component  {
+
+    
+        let settings = {
+            dots: true,
+            infinite: true,
+            speed: 50,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          };
+    
+  return (
+
+        <div>
+        <div className='Many_lesion'>
+        <div className="container mx-auto">
+            
+          {/* ROW_2 */}
+
+        <div data-slick='{"slidesToShow": 4, "slidesToScroll": 4}' >
+          <div className='row_2 '>
+            <h2 className='mt-8 mb-8 text-3xl font-semibold mb-4 '>Các khóa học được mua nhiều nhất</h2>
+
+            <div className="khoa_mua_nhieu_nhat flex gap-16 pb-8">
+              {/* 1 */}
+                  <div className='min-w-[171px] md:min-w-[285px]'>
+                     <a href="/vn/learning-path/combo-chinh-phuc-guitar-fingerstyle-tu-beginner-den-master-66fcfbb4ba37fa9dee1acc83">
+                  <img src="./public/c1.png" alt="" />
+                  </a>
+                  <h2 className='font-bold'>
+                  Combo. Chinh phục guitar fingerstyle (từ beginner đến)....
+                  </h2>
+                  <h3>2 khóa học</h3>
+                  <h4>đ399,00</h4>
+                </div>
+                {/* 2 */}
+                <div className='min-w-[171px] md:min-w-[285px]'>
+                     <a href="/vn/learning-path/combo-chinh-phuc-guitar-fingerstyle-tu-beginner-den-master-66fcfbb4ba37fa9dee1acc83">
+                  <img src="./public/c2.png" alt="" />
+                  </a>
+                  <h2 className='font-bold'>
+                  Combo. Chinh phục guitar fingerstyle (từ beginner đến)....
+                  </h2>
+                  <h3>2 khóa học</h3>
+                  <h4>đ399,00</h4>
+                </div>
+                  {/* 3 */}
+                  <div className='min-w-[171px] md:min-w-[285px]'>
+                     <a href="/vn/learning-path/combo-chinh-phuc-guitar-fingerstyle-tu-beginner-den-master-66fcfbb4ba37fa9dee1acc83">
+                  <img src="./public/c3.png" alt="" />
+                  </a>
+                  <h2 className='font-bold'>
+                  Combo. Chinh phục guitar fingerstyle (từ beginner đến)....
+                  </h2>
+                  <h3>2 khóa học</h3>
+                  <h4>đ399,00</h4>
+                </div>
+                  {/* 4 */}
+                  <div className='min-w-[171px] md:min-w-[285px]'>
+                     <a href="/vn/learning-path/combo-chinh-phuc-guitar-fingerstyle-tu-beginner-den-master-66fcfbb4ba37fa9dee1acc83">
+                  <img src="./public/c4.png" alt="" />
+                  </a>
+                  <h2 className='font-bold'>
+                  Combo. Chinh phục guitar fingerstyle (từ beginner đến)....
+                  </h2>
+                  <h3>2 khóa học</h3>
+                  <h4>đ399,00</h4>
+                </div>
+                    {/* 5 */}
+                    <div className='min-w-[171px] md:min-w-[285px]'>
+                     <a href="/vn/learning-path/combo-chinh-phuc-guitar-fingerstyle-tu-beginner-den-master-66fcfbb4ba37fa9dee1acc83">
+                  <img src="./public/c4.png" alt="" />
+                  </a>
+                  <h2 className='font-bold'>
+                  Combo. Chinh phục guitar fingerstyle (từ beginner đến)....
+                  </h2>
+                  <h3>2 khóa học</h3>
+                  <h4>đ399,00</h4>
+                </div>
+                    {/* 6 */}
+                    <div className='min-w-[171px] md:min-w-[285px]'>
+                     <a href="/vn/learning-path/combo-chinh-phuc-guitar-fingerstyle-tu-beginner-den-master-66fcfbb4ba37fa9dee1acc83">
+                  <img src="./public/c4.png" alt="" />
+                  </a>
+                  <h2 className='font-bold'>
+                  Combo. Chinh phục guitar fingerstyle (từ beginner đến)....
+                  </h2>
+                  <h3>2 khóa học</h3>
+                  <h4>đ399,00</h4>
+                </div>
+                    {/* 7 */}
+                    <div className='min-w-[171px] md:min-w-[285px]'>
+                     <a href="/vn/learning-path/combo-chinh-phuc-guitar-fingerstyle-tu-beginner-den-master-66fcfbb4ba37fa9dee1acc83">
+                  <img src="./public/c4.png" alt="" />
+                  </a>
+                  <h2 className='font-bold'>
+                  Combo. Chinh phục guitar fingerstyle (từ beginner đến)....
+                  </h2>
+                  <h3>2 khóa học</h3>
+                  <h4>đ399,00</h4>
+                </div>
+                    {/* 8 */}
+                    <div className='min-w-[171px] md:min-w-[285px]'>
+                     <a href="/vn/learning-path/combo-chinh-phuc-guitar-fingerstyle-tu-beginner-den-master-66fcfbb4ba37fa9dee1acc83">
+                  <img src="./public/c4.png" alt="" />
+                  </a>
+                  <h2 className='font-bold'>
+                  Combo. Chinh phục guitar fingerstyle (từ beginner đến)....
+                  </h2>
+                  <h3>2 khóa học</h3>
+                  <h4>đ399,00</h4>
+                </div>
+            </div>
+        </div>
+        </div>
+         {/* /////////////////////////////////////////////////// */}
+          {/* ROW_3 */}
+          <div className='row_3'>
+            <h2 className='mt-8 mb-8 text-3xl font-semibold mb-4'>Khóa học chất lượng cao</h2>
+
+            <div className="khoa_mua_nhieu_nhat flex gap-16 pb-8">
+              {/* 1 */}
+                  <div className='min-w-[171px] md:min-w-[285px]'>
+                     <a href="/vn/learning-path/combo-chinh-phuc-guitar-fingerstyle-tu-beginner-den-master-66fcfbb4ba37fa9dee1acc83">
+                  <img src="./public/d1.png" alt="" />
+                  </a>
+                  <h2 dlassName='font-bold'>
+                  dombo. Chinh phục guitar fingerstyle (từ beginner đến)....
+                  </h2>
+                  <h3>2 khóa học</h3>
+                  <h4>đ399,00</h4>
+                </div>
+                {/* 2 */}
+                <div className='min-w-[171px] md:min-w-[285px]'>
+                     <a href="/vn/learning-path/combo-chinh-phuc-guitar-fingerstyle-tu-beginner-den-master-66fcfbb4ba37fa9dee1acc83">
+                  <img src="./public/d2.png" alt="" />
+                  </a>
+                  <h2 className='font-bold'>
+                  Combo. Chinh phục guitar fingerstyle (từ beginner đến)....
+                  </h2>
+                  <h3>2 khóa học</h3>
+                  <h4>đ399,00</h4>
+                </div>
+                  {/* 3 */}
+                  <div className='min-w-[171px] md:min-w-[285px]'>
+                     <a href="/vn/learning-path/combo-chinh-phuc-guitar-fingerstyle-tu-beginner-den-master-66fcfbb4ba37fa9dee1acc83">
+                  <img src="./public/d3.png" alt="" />
+                  </a>
+                  <h2 className='font-bold'>
+                  Combo. Chinh phục guitar fingerstyle (từ beginner đến)....
+                  </h2>
+                  <h3>2 khóa học</h3>
+                  <h4>đ399,00</h4>
+                </div>
+                  {/* 4 */}
+                  <div className='min-w-[171px] md:min-w-[285px]'>
+                     <a href="/vn/learning-path/combo-chinh-phuc-guitar-fingerstyle-tu-beginner-den-master-66fcfbb4ba37fa9dee1acc83">
+                  <img src="./public/d4.png" alt="" />
+                  </a>
+                  <h2 className='font-bold'>
+                  Combo. Chinh phục guitar fingerstyle (từ beginner đến)....
+                  </h2>
+                  <h3>2 khóa học</h3>
+                  <h4>đ399,00</h4>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+        
+
+
+        <Slider {...settings}>
+      <div className='container mx-auto'>
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+      <div>
+        <h3>6</h3>
+      </div>
+    </Slider>
+    </div>
+        // 
+
+
+        
+  )
+}
+
+export default Many_lesion
+
+// 39:33
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Combo_Save from '../../components/Combo_Save/Combo_Save'
+import Many_lesion from '../../components/Section/Many_lesion'
 
 const Home = () => {
   return (
     <>
     <Header/>
     <Combo_Save/>
+    <Many_lesion/>
     </>
   )
 }
